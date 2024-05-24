515db565ae623a288fafd5288d40a95452db23cd
Haiyang S. Wang <30588030+astro-seanwhy@users.noreply.github.com>
2024-05-24 14:28:49 +0200

    Create 2sample_multivariate_MD.py

---
+++ 2sample_multivariate_MD.py
@@ -0,0 +1,14 @@
+"""
+A python script in implementing a 2-sample, multivariate Mahalanobis Distance signifiance test. 
+
+Written by: Haiyang S. Wang
+Date: 25-04-2024
+
+References: 
+Mahalanobis, P. C. 1930, J. Proceedings, Asiat. Soc. Bengal, 26, 541
+
+Mardia, K. V., Kent, J. T., & Bibby, J. M. 1979, in Probability and Mathematical
+Statistics, ed. Z. W. Birnbaum & E. Lukacs (London: Academic Press), 124
+
+https://online.stat.psu.edu/stat505/lesson/4/4.3
+""
