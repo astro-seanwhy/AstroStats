cbfdc10e8ab71431cd7c193151c70ade5f216616
Haiyang S. Wang <30588030+astro-seanwhy@users.noreply.github.com>
2024-05-24 14:36:28 +0200

    Update 2sample_multivariate_MD.py

--- 2sample_multivariate_MD.py
+++ 2sample_multivariate_MD.py
@@ -10,5 +10,7 @@ Mahalanobis, P. C. 1930, J. Proceedings, Asiat. Soc. Bengal, 26, 541
 Mardia, K. V., Kent, J. T., & Bibby, J. M. 1979, in Probability and Mathematical
 Statistics, ed. Z. W. Birnbaum & E. Lukacs (London: Academic Press), 124
 
+Mclachlan, G. J. 1999, Resonance, June, 20
+
 https://online.stat.psu.edu/stat505/lesson/4/4.3
 ""
